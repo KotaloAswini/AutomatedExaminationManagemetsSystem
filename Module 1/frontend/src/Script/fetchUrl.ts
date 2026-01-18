f4ba6efb71d57e6ebb17c596d445e61314fb9ded
Lalan Kumar <157386333+lalankumar17@users.noreply.github.com>
2026-01-18 19:13:23 +0530

    Delete Module 1 directory

--- Module 1/frontend/src/Script/fetchUrl.ts
+++
@@ -1,8 +0,0 @@
-declare global {
-    interface Window {
-        apiToken: string;
-    }
-}
-
-export const url = "http://localhost:8080"; // point to spring boot backend
-export const getApiToken = async () => "dummy-token"; // skip token check for now
