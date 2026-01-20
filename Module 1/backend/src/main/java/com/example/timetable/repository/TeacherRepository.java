eee4c4282fb89cc23d463ae53431b384aa124c6f
Lalan Kumar <157386333+lalankumar17@users.noreply.github.com>
2026-01-20 13:35:34 +0530

    Delete Module 1 directory

--- Module 1/backend/src/main/java/com/example/timetable/repository/TeacherRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.timetable.repository;
-
-import com.example.timetable.model.Teacher;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TeacherRepository extends MongoRepository<Teacher, String> {
-}
