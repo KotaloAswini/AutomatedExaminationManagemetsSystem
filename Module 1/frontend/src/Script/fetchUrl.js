e799b193b8048572b283029990e075b736a86752
Lalan Kumar <157386333+lalankumar17@users.noreply.github.com>
2026-01-20 17:47:03 +0530

    Delete Module 1 directory

--- Module 1/frontend/src/Script/fetchUrl.js
+++
@@ -1,2 +0,0 @@
-export const url = import.meta.env.VITE_API_BASE_URL || "http://localhost:8080"; // point to spring boot backend
-export const getApiToken = async () => "dummy-token"; // skip token check for now
