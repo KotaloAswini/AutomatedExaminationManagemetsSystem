2b85ac401ab7af5cbe3089ef885e531aab801722
anilsoul <anilkumark00218@gmail.com>
2026-01-30 00:00:47 +0530

    Delete module5 directory

--- module5/backend/src/main/java/com/nmit/dutyexchange/repository/DutyExchangeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.nmit.dutyexchange.repository;
-
-import com.nmit.dutyexchange.model.DutyExchange;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DutyExchangeRepository extends MongoRepository<DutyExchange, String> {
-}
--- module5/frontend/src/components/README.md
+++
@@ -1,9 +0,0 @@
-# Duty Exchange System
-
-![Dashboard](./images/dashboard.png)
-
-![Duty Exchange Form](./images/duty_exchange_form.png)
-
-![View DES Form](./images/view_des_form.png)
-
-![Settings](./images/settings.png)
