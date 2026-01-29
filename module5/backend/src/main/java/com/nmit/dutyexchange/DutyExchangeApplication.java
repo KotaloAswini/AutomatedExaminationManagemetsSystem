9de731463d77d323cbf2dc6dc00eeba3b761a11e
anilsoul <anilkumark00218@gmail.com>
2026-01-29 23:41:48 +0530

    module5

---
+++ module5/backend/src/main/java/com/nmit/dutyexchange/DutyExchangeApplication.java
@@ -0,0 +1,13 @@
+package com.nmit.dutyexchange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DutyExchangeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DutyExchangeApplication.class, args);
+	}
+
+}
